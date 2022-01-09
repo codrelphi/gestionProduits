3e6c78998f42799d81b32e5766c2fc43139afb31
codrelphi <chancerel.codjovi@gmail.com>
2022-01-09 22:52:37 +0100

    add GestionProduit.java and Produit.java

--- src/gestionProduits/GestionProduit.java
+++ src/gestionProduits/GestionProduit.java
@@ -1,5 +1,7 @@
 package gestionProduits;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GestionProduit {
@@ -27,17 +29,69 @@ public class GestionProduit {
 		return reponse;
 	}
 	public static void main(String[] args) {
+		
 		String reponse;
 		reponse = menu();
-		System.out.println("\n\nVotre réponse est: " + reponse);
+		//System.out.println("\n\nVotre réponse est: " + reponse);
 		
-		/*switch (reponse) {
-		case "liste":
-			//afficher la liste
-			break;
+		List<Produit> produits = new ArrayList<Produit>();
 		
+		String gCode, gDescription;
+		double gPrix;
 		
-		}*/
+		switch (reponse) {
+			case "liste":
+				//afficher la liste
+				if (produits.isEmpty()) {
+					System.out.println("Il n'y a aucun produit. Veuillez ajouter un produit");
+				}
+				else {
+					System.out.println("LISTE DES PRODUITS");
+					for (Produit p : produits) {
+						System.out.println(p.getCode() + "\t" + p.getDescription() + "\t" + p.getPrix());
+					}
+				}
+				
+				break;
+			case "ajoute":
+				// ajouter
+				System.out.print("\nEntrer code du produit: ");
+				gCode = sc.next();
+			
+				System.out.print("Entrer description du produit: ");
+				gDescription = sc.next();
+				
+				System.out.print("Entrer prix du produit: ");
+				gPrix = sc.nextDouble();
+				
+				Produit tempProduit = new Produit(gCode, gDescription, gPrix);
+				
+				String messageAfficher = "";
+				if(produits.add(tempProduit)) {
+					messageAfficher = "<" + gDescription + "> a été bien ajouté.";
+				}
+				else {
+					messageAfficher = gDescription + " n'a pas été ajouté.";
+				}
+				
+				System.out.println(messageAfficher);
+				//reponse = menu();
+				
+				
+				break;
+			case "suppr":
+				//supprimer
+				break;
+			case "aide":
+				//afficher l'aide
+				break;
+			case "quitter":
+				//quitter
+				break;
+			default:
+				System.out.println("Veuillez faire un choix !");
+		
+		}
 		
 		
 	}
