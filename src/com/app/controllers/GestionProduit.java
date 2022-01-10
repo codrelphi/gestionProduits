f59e9170cee6a4e3747f2415a545fb5c9c54e251
codrelphi <chancerel.codjovi@gmail.com>
2022-01-10 22:43:46 +0100

    réorganiser le code avec les packages <controllers> et <models>

---
+++ src/com/app/controllers/GestionProduit.java
@@ -0,0 +1,122 @@
+package com.app.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.app.models.Produit;
+
+public class GestionProduit {
+
+	private static Scanner sc = new Scanner(System.in);
+	private static List<Produit> produits = new ArrayList<Produit>();
+	private static String gCode;
+	private static String gDescription;
+	private static double gPrix;
+	
+	static void menu() {
+		boolean leTest;
+		String reponse;
+		do {
+			System.out.println("------------------------------------------------------");
+			System.out.println("Bienvenue dans l'application <Gestion de Produits>\n");
+			System.out.println("COMMANDES DU MENU");
+			System.out.println("liste\t - Liste tous les produits");
+			System.out.println("ajoute\t - Ajoute un produit");
+			System.out.println("suppr\t - Supprime un produit");
+			System.out.println("aide\t - Affiche ce menu");
+			System.out.println("quitter\t - Quitte l'application");
+			System.out.print("\nEntrer une commande: ");
+			reponse = sc.next();
+			reponse = reponse.trim().toLowerCase();
+			leTest = !(reponse.equals("liste") || reponse.equals("ajoute") || reponse.equals("suppr") || reponse.equals("aide") || reponse.equals("quitter"));
+			//System.out.println("<< " + leTest + ">>");
+		} while (leTest);
+		
+		//return reponse;
+		switch (reponse) {
+			case "liste":
+				//afficher la liste
+				if (produits.isEmpty()) {
+					System.out.println("Il n'y a aucun produit. Veuillez ajouter un produit");
+				}
+				else {
+					System.out.println("LISTE DES PRODUITS");
+					for (Produit p : produits) {
+						System.out.println(p.getCode() + "\t" + p.getDescription() + "\t" + p.getPrix());
+					}
+				}
+				break;
+			case "ajoute":
+				// ajouter
+				System.out.print("\nEntrer code du produit: ");
+				gCode = sc.next();
+			
+				System.out.print("Entrer description du produit: ");
+				gDescription = sc.nextLine();
+				sc.nextLine();
+				
+				//System.out.println("Description: " + gDescription);
+				
+				System.out.print("Entrer prix du produit: ");
+				gPrix = sc.nextDouble();
+				
+				Produit tempProduit = new Produit(gCode, gDescription, gPrix);
+				
+				String messageAfficher = "";
+				if(produits.add(tempProduit)) {
+					messageAfficher = "<" + gDescription + "> a été bien ajouté.";
+				}
+				else {
+					messageAfficher = gDescription + " n'a pas été ajouté.";
+				}
+				
+				System.out.println(messageAfficher);
+				//reponse = menu();
+				
+				
+				break;
+			case "suppr":
+				//supprimer
+				break;
+			case "aide":
+				//afficher l'aide (cad le menu)
+				menu();
+				break;
+			case "quitter":
+				//quitter
+				char repChar;
+				do {
+					System.out.print("Voulez-vous vraiment quitter ? (O/N)");
+					String rep = sc.next().toUpperCase();
+					repChar = rep.charAt(0);
+					if(repChar == 'O') {
+						System.out.println("Au revoir !");
+						System.exit(0);
+					}
+					if (repChar == 'N') {
+						menu();
+					}
+					
+				} while(repChar != 'O' && repChar != 'N');
+				break;
+			default:
+				System.out.println("Veuillez faire un choix !");
+		
+		}
+		menu();
+	}
+	public static void main(String[] args) {
+		
+		//String reponse;
+		//reponse = menu();
+		//System.out.println("\n\nVotre réponse est: " + reponse);
+	
+		/*String gCode, gDescription;
+		double gPrix;*/
+		
+		menu();
+		
+		
+	}
+}
---
+++ src/com/app/models/Produit.java
@@ -0,0 +1,46 @@
+package com.app.models;
+
+public class Produit {
+
+	private String code;
+	private String description;
+	private double prix;
+	
+	public Produit() {}
+	
+	public Produit(String c, String d, double p) {
+		this.code = c;
+		this.description = d;
+		this.prix = p;
+	}
+
+	@Override
+	public String toString() {
+		return "Produit [code=" + code + ", description=" + description + ", prix=" + prix + "]";
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+}
